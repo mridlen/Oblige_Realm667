4c7915c114ec2d6c9210a463ecbe1218d051099c
Mark Ridlen <mark@ridlen.net>
2020-06-10 17:28:51 -0500

    weapons are ALPHA - broken

---
+++ modules/realm667_weapons.lua
@@ -0,0 +1,1265 @@
+REALM667W = { }
+
+REALM667W.WEAPONS =
+{	
+	GrenadeLauncher40mm =
+	{
+		id = 29010
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+		give = { {ammo="shell",count=8} }
+	}
+	AA12 =
+	{
+		id = 29604
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	ActionMachineGun =
+	{
+		id = 29124
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	AK47 =
+	{
+		id = 29125
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	AlphaUnmaker =
+	{
+		id = 29263
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	AlphaUnmaker =
+	{
+		id = 29266
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	AngledPistol =
+	{
+		id = 29128
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Autogun =
+	{
+		id = 29130
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	AutoShot =
+	{
+		id = 5664
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Axe =
+	{
+		id = 29131
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	BayonetRifle =
+	{
+		id = 29132
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	BFG2704 =
+	{
+		id = 29140
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	BHGen =
+	{
+		id = 29150
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	BioPipebombLauncher =
+	{
+		id = 29141
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Butchergun =
+	{
+		id = 29151
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Channeler =
+	{
+		id = 29152
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	ClaymoreMine =
+	{
+		id = 10200
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Coachgun =
+	{
+		id = 29160
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Colt45 =
+	{
+		id = 5757
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	CryoBow =
+	{
+		id = 29161
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	D2C_Pipebomb =
+	{
+		id = 25018
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	DemonicLeech =
+	{
+		id = 29170
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	DGL =
+	{
+		id = 29173
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	DoubleBladedChainsaw =
+	{
+		id = 29172
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	DualLaser =
+	{
+		id = 5666
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	DukeShotgun =
+	{
+		id = 29174
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	EgoSmasher =
+	{
+		id = 29175
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	ElectroGun =
+	{
+		id = 29176
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	FistRedux =
+	{
+		id = 29178
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	FlashLightAngled =
+	{
+		id = 29182
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	FragGrenade =
+	{
+		id = 11907
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	FreezeRifle =
+	{
+		id = 29183
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	GasGun =
+	{
+		id = 20039
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	GatLaser =
+	{
+		id = 6570
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Glock18 =
+	{
+		id = 29184
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	HandGrenade =
+	{
+		id = 25099
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	HeavyCarbine =
+	{
+		id = 20212
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	HeavyRifle =
+	{
+		id = 25035
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	HellstormCannon =
+	{
+		id = 29187
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	iGun =
+	{
+		id = 29191
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Ionspray =
+	{
+		id = 29192
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Karasawa =
+	{
+		id = 29193
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	KDiZDGrenadeLauncher =
+	{
+		id = 29186
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	KDiZDSuperShotgun =
+	{
+		id = 29257
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Knife =
+	{
+		id = 29194
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	LandMineLayer =
+	{
+		id = 29195
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	LaserCannon =
+	{
+		id = 29197
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	M16 =
+	{
+		id = 29198
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	M40Rifle =
+	{
+		id = 29199
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	M79 =
+	{
+		id = 29202
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Machete =
+	{
+		id = 29205
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Machinegun =
+	{
+		id = 29206
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Mag60 =
+	{
+		id = 5509
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	MancubusArm =
+	{
+		id = 29207
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	MicroUzi =
+	{
+		id = 29606
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	model1887 =
+	{
+		id = 29210
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	MP40 =
+	{
+		id = 12400
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	MP5 =
+	{
+		id = 29211
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	NailGunMG =
+	{
+		id = 29212
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	NailgunSG =
+	{
+		id = 29214
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	NuclearMissileLauncher =
+	{
+		id = 29608
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	PlasmaBallista =
+	{
+		id = 29217
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	PlasmaBeam =
+	{
+		id = 29218
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	PlasmaBolter =
+	{
+		id = 29219
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	PlasmaPistol =
+	{
+		id = 29220
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	PlasmaRepeater =
+	{
+		id = 5012
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	PlasmaShotgun =
+	{
+		id = 29221
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	PulseNailgun =
+	{
+		id = 29609
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	PulseRifle =
+	{
+		id = 18730
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	PumpActionShotgun =
+	{
+		id = 29227
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	QuadShotgun =
+	{
+		id = 29999
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Raptor =
+	{
+		id = 29230
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Raygun =
+	{
+		id = 12634
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Reaper =
+	{
+		id = 6021
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Repeater =
+	{
+		id = 29231
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Rifle =
+	{
+		id = 25096
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Rifle =
+	{
+		id = 29235
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	RivetGun =
+	{
+		id = 7932
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	ScatterPistol =
+	{
+		id = 29238
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	ShieldGun =
+	{
+		id = 29239
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	SidebySide =
+	{
+		id = 29240
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	SilencedPistol =
+	{
+		id = 29241
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	SingleShot =
+	{
+		id = 29242
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	SkeletalRevenge =
+	{
+		id = 29243
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Smartgun =
+	{
+		id = 29250
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Smasher =
+	{
+		id = 12465
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	SniperRifle =
+	{
+		id = 2395
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	SPAS12 =
+	{
+		id = 29251
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	SprayCannon =
+	{
+		id = 29252
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	STBFG10K =
+	{
+		id = 29136
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	STGrenadeLauncher =
+	{
+		id = 29185
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	STMinigun =
+	{
+		id = 29607
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	STRailgun =
+	{
+		id = 29229
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	SuppressedMP5 =
+	{
+		id = 29258
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	SwatShotgun =
+	{
+		id = 29259
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Thompson =
+	{
+		id = 29260
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Toaster =
+	{
+		id = 12466
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	UACPlasmaticRifle =
+	{
+		id = 29261
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	UberMinigun =
+	{
+		id = 29262
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	UTNTFlamethrower =
+	{
+		id = 29179
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	UTNTPyroCannon =
+	{
+		id = 29269
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Uzi =
+	{
+		id = 20002
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	WensternShotgun =
+	{
+		id = 29272
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+	Yellowboy =
+	{
+		id = 22911
+		level = 1
+		pref = 40
+		add_prob = 5
+		attack = "hitscan"
+		rate = 0.9
+		damage = 50
+		ammo = "bullet"
+		per = 1
+	}
+}
+
+OB_MODULES["REALM667W"] =
+{
+  label = "Realm667 Weapons"
+
+  tables =
+  {
+    REALM667W
+  }
+}
+
